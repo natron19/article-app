bd65b26efcb149433765907d9fa8404d4d202cdb
natron19 <nathan19@hotmail.com>
2016-11-05 23:10:42 -0700

    adds fields to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  resources :pages
+  devise_for :users, :controllers => { registrations: 'registrations' }
+
   root 'pages#index'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
+  get 'pages/contact'
+  get 'pages/about'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
