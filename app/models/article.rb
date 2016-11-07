73627c940394911a9e4dfcde924a2bc50ba91c3c
natron19 <nathan19@hotmail.com>
2016-11-06 18:42:08 -0800

    validations for view

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,8 @@ class Article < ActiveRecord::Base
 
   has_many :comments
 
+  validates_presence_of :title
+  validates :title, uniqueness: true, uniqueness: { message: "This article title has already been posted."}
+  validates :title, length: { minimum: 5}
+  validates :body, length: { minimum: 10}
 end
