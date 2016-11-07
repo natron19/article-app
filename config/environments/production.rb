3309ead7f72a32cc99f0a1ced9298d7ff73a5a3a
natron19 <nathan19@hotmail.com>
2016-11-06 19:28:47 -0800

    adds gmail credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://nate-articles.herokuapp.com/', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://natron-article-app.herokuapp.com/' }
 end
